1a4e853af9b2d463d5ce1eba21e28ddcafeee2f6
Jose Nuñez <96750637+Josenzb@users.noreply.github.com>
2023-02-13 13:30:29 -0500

    Se crean registros para las tablas de libreria

---
+++ Taller 5/Actividad 1/Insertar datos cliente.sql
@@ -0,0 +1,15 @@
+SELECT * FROM libreriabuscalibre.cliente;
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`) 
+VALUES ('784521', 'Andres perez');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`)
+ VALUES ('898956', 'David Lopez');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`) 
+VALUES ('454565', 'Claudia Gomez');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`) 
+VALUES ('121232', 'Estafania Tejada');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`) 
+VALUES ('651234', 'Sol Castro');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`) 
+VALUES ('979746', 'Jesica Martines');
+INSERT INTO `libreriabuscalibre`.`cliente` (`cedula`, `nombre`)
+ VALUES ('141564', 'Jorge Mendez');
