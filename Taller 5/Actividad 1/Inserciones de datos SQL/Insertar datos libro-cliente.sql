96919c56c03e518e9fee4448eb0dc4155ca70337
Jose Nuñez <96750637+Josenzb@users.noreply.github.com>
2023-02-13 13:32:50 -0500

    Se crea carpeta inserciones para mejor orden

---
+++ Taller 5/Actividad 1/Inserciones de datos SQL/Insertar datos libro-cliente.sql
@@ -0,0 +1,22 @@
+SELECT * FROM libreriabuscalibre.libro_cliente;
+
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('111-111', '121232');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('333-333', '141564');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('555-555', '454565');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('666-666', '651234');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('999-999', '784521');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('000-000', '898956');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('111-111', '979746');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('000-000', '979746');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('202-202', '121232');
+INSERT INTO `libreriabuscalibre`.`libro_cliente` (`ISBN_libro_cliente`, `id_cliente`) 
+VALUES ('111-111', '141564');
---
+++ Taller 5/Actividad 1/Inserciones de datos SQL/Insertar datos telefono-cliente.sql
@@ -0,0 +1,25 @@
+SELECT * FROM libreriabuscalibre.telefono_cliente;
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('121232', '3100300');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('121232', '4500500');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('141564', '8600600');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('454565', '4900900');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('651234', '4100100');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('784521', '3200200');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('898956', '5300300');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('979746', '6200200');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('651234', '7800800');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('651234', '9800800');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('141564', '6500540');
+INSERT INTO `libreriabuscalibre`.`telefono_cliente` (`cedula_cliente`, `numero`) 
+VALUES ('454565', '8450450');
