483ac5cb3515e3c77a2eecce621e0194bf7073db
Ivan <idruizbe@gmail.com>
2023-02-13 16:23:42 -0500

    Se añaden los primeros registros y se empieza la documentación

---
+++ Inserción 5 autores.sql
@@ -0,0 +1,6 @@
+INSERT INTO autor (id, fecha_nacimiento, nacionalidad, nombre)
+VALUES (1, '1899-08-24', 'Argentina', 'Jorge Luis Borges'),
+       (2, '1898-06-05', 'España', 'Federico García Lorca'),
+       (3, '1914-03-31', 'Mexico', 'Octavio Paz'),
+       (4, '1914-08-26', 'Argentina', 'Julio Cortázar'),
+       (5, '1927-03-06', 'Colombia', 'Gabriel García Márquez');
