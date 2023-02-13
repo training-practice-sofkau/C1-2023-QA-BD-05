6916f41c9f3d9cf41e8b57164cd9266bc70a76ce
fjrrodriguez <103499132+fjrrodriguez@users.noreply.github.com>
2023-02-13 13:58:33 -0500

    Sentencias SQL cargar registros en tablas
    Se  generan las sentencias SQL que permiten cargar los registros en cada una de las tablas, se adjunta documentación y documento en formato SQL

---
+++ TALLER 13-02/Ejercicio 1/Sentencias SQL para cargar registros en tablas.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: select * from libreriabuscalibre.telefono_cliente
+LIMIT 0, 1000
+
+-- Date: 2023-02-13 13:54
+*/
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('1053874','30066254');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('1053874','31398748');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('10653870','36456535');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('10653870','76835654');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('12985876','8672451');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('12985876','8676514');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('22982870','31265344');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('22982870','32177635');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('31538744','30076545');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('31538744','30076642');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('78653877','30176364');
+INSERT INTO `` (`cedula_cliente`,`numero`) VALUES ('78653877','3227536');
