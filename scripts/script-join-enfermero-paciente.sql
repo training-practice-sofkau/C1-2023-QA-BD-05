df98e785d5289213ca2419ead38c7153dcea99af
SergioBalca <SergioBalca@github.com>
2023-02-14 16:17:31 -0500

    Se anexa el archivo que contiene script para crear vista que permite visualizar los procedimientos realizados a los pacientes, con el nombre del médico que los realiza

--- scripts/script-join-enfermero-paciente.sql
+++ scripts/script-join-enfermero-paciente.sql
@@ -3,3 +3,5 @@ FROM 	procedimiento_medico.enfermero
 		INNER JOIN procedimiento_medico.medico_procedimiento ON enfermero.medico_id = medico_procedimiento.medico_id
         INNER JOIN procedimiento_medico.procedimiento ON procedimiento_id = procedimiento.id
         INNER JOIN procedimiento_medico.paciente ON procedimiento_id = paciente.id_procedimiento;
+        
+        
