55d62d49b7f1e0c7fbcebdb6b3887fcd3a8e97a2
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2023-02-14 01:38:30 -0500

    Actualizacion consultas hospital, actualizacion documentacion

--- Segunda Actividad/Consultas Hospital.sql
+++ Segunda Actividad/Consultas Hospital.sql
@@ -29,6 +29,7 @@ FROM
  -- -----------------------------------------------------
 -- Historial medico de los pacientes
 -- -----------------------------------------------------
+CREATE VIEW Historial_Medico AS
   SELECT 
   tb_paciente.nombre_paciente AS 'Paciente', 
   tb_procedimiento.tipo_procedimiento AS 'Procedimiento',
@@ -40,18 +41,21 @@ FROM
   JOIN dll_paciente_medicamento ON tb_paciente.id_paciente = dll_paciente_medicamento.id_paciente
   JOIN tb_medicamento ON dll_paciente_medicamento.id_medicamento = tb_medicamento.id_medicamento;
 
-  
  -- -----------------------------------------------------
 -- valor Facturacion segun procedimiento
 -- -----------------------------------------------------
+CREATE VIEW Facturacion_segun_procedimiento AS
 SELECT
-tb_paciente.nombre_paciente AS 'Paciente',
-tb_procedimiento.tipo_procedimiento AS 'Procedimiento',
-tb_factura.valor_total AS 'Factura'
-FROM
-tb_paciente
-JOIN tb_procedimiento ON tb_paciente.id_procedimiento = tb_procedimiento.id_procedimiento
-JOIN tb_factura ON tb_paciente.id_paciente = tb_factura.id_paciente;
+  tb_paciente.nombre_paciente AS 'Paciente',
+   telefono_paciente.telefono AS 'Teléfono',
+  tb_procedimiento.tipo_procedimiento AS 'Procedimiento',
+  tb_factura.valor_total AS 'Factura'
+ FROM
+  tb_paciente
+  JOIN tb_procedimiento ON tb_paciente.id_procedimiento = tb_procedimiento.id_procedimiento
+  JOIN tb_factura ON tb_paciente.id_paciente = tb_factura.id_paciente
+  JOIN telefono_paciente ON tb_paciente.id_paciente = telefono_paciente.id_paciente;
+
 
 
 
