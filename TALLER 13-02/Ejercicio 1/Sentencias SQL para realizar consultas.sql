501127fb55dd303ab3ae3c76eb1736dd9b6d86fe
fjrrodriguez <103499132+fjrrodriguez@users.noreply.github.com>
2023-02-13 19:49:16 -0500

    Se crean las vistas
    Se actualiza documentación donde se evidencia la creacion de las vistas seleccionadas, se adjunta documento SQL con actualización incluyendo vistas

--- TALLER 13-02/Ejercicio 1/Sentencias SQL para realizar consultas.sql
+++ TALLER 13-02/Ejercicio 1/Sentencias SQL para realizar consultas.sql
@@ -1,14 +1,13 @@
 /*
--- Query: select cliente.nombre, count(libro_cliente.ISBN_libro_cliente) as Libros_Vendidos_Por_LibreriaBuscaLibre
-from libro_cliente inner join cliente
-on libro_cliente.id_cliente = cliente.cedula
-group by cliente.nombre
+-- Query: select * from LibrosVendidos
 LIMIT 0, 1000
 
--- Date: 2023-02-13 17:51
+-- Date: 2023-02-13 19:46
 */
-INSERT INTO `` (`nombre`,`Libros_Vendidos_Por_LibreriaBuscaLibre`) VALUES (Faber Mora,2);
-INSERT INTO `` (`nombre`,`Libros_Vendidos_Por_LibreriaBuscaLibre`) VALUES (Fabian Torres,2);
-INSERT INTO `` (`nombre`,`Libros_Vendidos_Por_LibreriaBuscaLibre`) VALUES (Francy Jimenez,2);
-INSERT INTO `` (`nombre`,`Libros_Vendidos_Por_LibreriaBuscaLibre`) VALUES (Gonzalo Mora,2);
-INSERT INTO `` (`nombre`,`Libros_Vendidos_Por_LibreriaBuscaLibre`) VALUES (Feiber Torres,2);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('El dia mas feliz',2);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Ciencia y numeros',2);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Cien enanos',1);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Casa y hogar',1);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Cien Colombianos dicen',1);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Ciencia y tecnologia',1);
+INSERT INTO `` (`titulo`,`libros_vendidos`) VALUES ('Ciencia y arte',1);
